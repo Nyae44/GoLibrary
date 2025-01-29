cf6696d78d22aefc3175d484c8a7aeedc08e999f
Nyae44 <tingadaley@gmail.com>
2025-01-29 15:35:11 +0300

    add models:book,member,user and transaction

---
+++ internal/models/member.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Member struct {
+	gorm.Model
+	UserID      uint    `gorm:"not null" json:"user_id"`
+	User        User    `gorm:"foreignkey:UserID" json:"user"`
+	PhoneNumber string  `gorm:"unique" json:"phone_number"`
+	Email       string  `gorm:"unique" json:"email"`
+	RentalDebt  float64 `gorm:"default:0" json:"rental_debt"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
+
+func (m *Member) AddToRentalDebt(db *gorm.DB, amount float64) error {
+	m.RentalDebt += amount
+	return db.Save(m).Error
+}
